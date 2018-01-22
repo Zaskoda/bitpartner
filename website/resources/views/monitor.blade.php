8d09d5e1a422af254920831437eaadeed911894e
zasKoda <zaskoda@gmail.com>
2018-01-21 21:34:21 -0800

    better formatting

--- website/resources/views/monitor.blade.php
+++ website/resources/views/monitor.blade.php
@@ -6,57 +6,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Bit Partner</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #000000;
-                font-family: 'Raleway', sans-serif;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
             }
 
             .content {
                 margin: 10px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
@@ -73,16 +40,26 @@
             @endif
 
             <div class="content">
-
-                @foreach($readings as $reading)
-                    <div>
-                    <span style="background-color: rgb({{ $reading->rgb }})">{{ $reading->lux }}</span> |
-                    {{ $reading->timestamp }} |
-                    {{ $reading->temperature }} |
-                    <b>{{ $reading->tempInF() }}</b>
-                    <b></b>
-                    </div>
-                @endforeach
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Color</th>
+                            <th>Time</th>
+                            <th>Temp</th>
+                            <th>Temp</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($readings as $reading)
+                        <tr>
+                            <td style="background-color: rgb({{ $reading->rgb }})">{{ $reading->lux }}</td>
+                            <td>{{ Carbon\Carbon::parse($reading->timestamp)->format('m-d H:i') }}</td>
+                            <td>{{ round($reading->temperature,2) }} C</td>
+                            <td>{{ $reading->tempInF() }} F</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </body>
