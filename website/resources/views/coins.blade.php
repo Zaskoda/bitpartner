2a3945bd8a46802a03b61e323b2d005cd24a13f0
zasKoda <zaskoda@gmail.com>
2018-03-04 22:26:52 -0800

    tighter table

--- website/resources/views/coins.blade.php
+++ website/resources/views/coins.blade.php
@@ -2,19 +2,15 @@
 
 @section('content')
 
-    <table class="table table-hover">
+    <table class="table table-hover table-condensed">
         <thead>
             <tr>
                 <th></th>
                 <th>Name</th>
-                <th>Symbol</th>
                 <th>Genesis Date</th>
                 <th>Creator</th>
                 <th>Summary</th>
                 <th>Links</th>
-                @auth
-                <th class="text-right"><a href="/coins/create" class="btn btn-xs btn-success">new</a></th>
-                @endauth
             </tr>
         </thead>
         <tbody>
@@ -24,17 +20,15 @@
                     @if($coin->logo) <img src="{{ $coin->logo }}" style="height: 1em"> @endif
                 </td>
                 <td>
+                    <span class="badge"><small> {{ $coin->symbol }} </small> </span>
                     <b>{{ $coin->name }}</b> 
-                </td>
-                <td>
-                <span class="badge"> {{ $coin->symbol }} </span> 
                 </td>
                 <td>{{ \Carbon\Carbon::createFromFormat('Y-m-d',$coin->genesis_date)->toFormattedDateString() }}</td>
                 <td>{{ $coin->creator }}</td>
-                <td>{{ $coin->summary }}</td>
+                <td><small>{{ $coin->summary }}</small></td>
                 <td>
                     <div class="btn-group">
-
+                        <a class="btn btn-default btn-xs" href="/coins/{{ $coin->id }}"><i class="fa fa-fw fa-eye"></i></a>
                         @if($coin->website)<a class="btn btn-default btn-xs" href="{{ $coin->website }}"><i class="fa fa-fw fa-home"></i></a>@endif
                         @if($coin->source)<a class="btn btn-default btn-xs" href="{{ $coin->source }}"><i class="fa fa-fw fa-github-alt"></i></a>@endif
                         @if($coin->paper)<a class="btn btn-default btn-xs" href="{{ $coin->paper }}"><i class="fa fa-fw fa-file"></i></a>@endif
@@ -42,12 +36,11 @@
                         @if($coin->reddit)<a class="btn btn-default btn-xs" href="{{ $coin->reddit }}"><i class="fa fa-fw fa-reddit"></i></a>@endif
                         @if($coin->wikipedia)<a class="btn btn-default btn-xs" href="{{ $coin->wikipedia }}"><i class="fa fa-fw fa-wikipedia-w"></i></a>@endif
                         @if($coin->docs)<a class="btn btn-default btn-xs" href="{{ $coin->docs }}"><i class="fa fa-fw fa-book"></i></a>@endif
-                        <a class="btn btn-default btn-xs" href="/coins/{{ $coin->id }}"><i class="fa fa-fw fa-eye"></i></a>
+                        @auth
+                        <a href="/coins/{{ $coin->id }}/edit" class="btn btn-xs btn-default"><i class="fa fa-fw fa-pencil"></i></a></td>
+                        @endauth
                     </div>
                 </td>
-                @auth
-                <td class="text-right"><a href="/coins/{{ $coin->id }}/edit" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>
-                @endauth
             </tr>
             @endforeach
         </tbody>
