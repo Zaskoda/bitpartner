cf92410c154ad027e6b55b79b9f95e75dd612bb8
zasKoda <zaskoda@gmail.com>
2018-03-05 11:09:47 -0800

    added tracking

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -87,4 +87,14 @@
             </div>
         </div>
     </body>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115052305-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-115052305-1');
+    </script>
+
 </html>
