a4cf91b7540f32304c1757ed030a1b6ada513708
zasKoda <zaskoda@gmail.com>
2018-03-09 18:09:15 -0800

    better spacing

--- website/resources/views/coins.blade.php
+++ website/resources/views/coins.blade.php
@@ -7,16 +7,16 @@
             <table class="table table-hover table-condensed">
                 <thead>
                     <tr>
-                        <th width="5%"></th>
-                        <th width="5%" class="text-right">Sym</th>
-                        <th width="10%" >Name</th>
-                        <th width="10%">Genesis Date</th>
+                        <th width="3%"></th>
+                        <th width="4%" class="text-right">Sym</th>
+                        <th width="9%" >Name</th>
+                        <th width="8%">Genesis</th>
                         <th width="10%">Creator</th>
-                        <th width="25%">Summary</th>
-                        <th width="33%">
+                        <th width="26%">Summary</th>
+                        <th width="34%">
                             Links
                         </th>
-                        <th width="2%" class="text-right">
+                        <th width="6%" class="text-right">
                             @role('sysop')
                             <a href="/coins/create" class="btn btn-xs btn-success"><i class="fa fa-fw fa-plus"></i></a>
                             @endrole
@@ -26,19 +26,19 @@
                 <tbody>
                     @foreach ($coins as $coin)
                     <tr class='clickable-row' onclick="window.location = '/coins/{{ $coin->id }}'" style=" cursor: pointer; ">
-                        <td width="5%" class="text-center">
+                        <td width="3%" class="text-center">
                             @if($coin->logo) <a href="{{ $coin->logo }}"><img src="{{ $coin->logo }}" style="height: 1.5em"></a> @endif
                         </td>
-                        <td width="5%" class="text-right">
+                        <td width="4%" class="text-right">
                             <span class="badge"><small> {{ $coin->symbol }} </small> </span>
                         </td>
-                        <td width="10%">
+                        <td width="9%">
                             <b>{{ $coin->name }}</b> 
                         </td>
-                        <td width="10%"><small>@if($coin->genesis_date){{ \Carbon\Carbon::createFromFormat('Y-m-d',$coin->genesis_date)->toFormattedDateString() }}@endif</small></td>
+                        <td width="8%"><small>@if($coin->genesis_date){{ \Carbon\Carbon::createFromFormat('Y-m-d',$coin->genesis_date)->toFormattedDateString() }}@endif</small></td>
                         <td width="10%"><small>{{ $coin->creator }}</small></td>
-                        <td width="25%"><small>{{ $coin->summary }}</small></td>
-                        <td width="33%">
+                        <td width="26%"><small>{{ $coin->summary }}</small></td>
+                        <td width="34%">
                             <div class="btn-group">
                                 @if($coin->website)<a class="btn btn-default btn-xs" href="{{ $coin->website }}"><i class="fa fa-fw fa-home"></i></a>@endif
                                 @if($coin->source)<a class="btn btn-default btn-xs" href="{{ $coin->source }}"><i class="fa fa-fw fa-github-alt"></i></a>@endif
@@ -49,13 +49,11 @@
                                 @if($coin->docs)<a class="btn btn-default btn-xs" href="{{ $coin->docs }}"><i class="fa fa-fw fa-book"></i></a>@endif
                             </div>
                         </td>
-                        <td width="2%" class="text-right">
-                            <div class="btn-group">
-                                <a class="btn btn-default btn-xs" href="/coins/{{ $coin->id }}"><i class="fa fa-fw fa-eye"></i></a>
-                                @role('sysop')
-                                <a href="/coins/{{ $coin->id }}/edit" class="btn btn-xs btn-default"><i class="fa fa-fw fa-pencil"></i></a></td>
-                                @endrole
-                            </div>
+                        <td width="6%" class="text-right">
+                            <a class="btn btn-default btn-xs" href="/coins/{{ $coin->id }}"><i class="fa fa-fw fa-eye"></i></a>
+                            @role('sysop')
+                            <a href="/coins/{{ $coin->id }}/edit" class="btn btn-xs btn-default"><i class="fa fa-fw fa-pencil"></i></a></td>
+                            @endrole
                         </td>
                     </tr>
                     @endforeach
